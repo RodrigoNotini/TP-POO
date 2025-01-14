fd671cd4dc5f827021e3dc412cac3a35d45fd7d2
Rodrigo <rodrigo.ferr.not@gmail.com>
2025-01-13 23:45:30 -0300

    novos cheats

--- Carrega.py
+++ Carrega.py
@@ -9,7 +9,7 @@ pygame.mixer.init()
 
 # Carrega Imagens
 try:
-    MENU_BG_IMAGE = pygame.image.load("./images/menu_bg.jpg")  # For the menu
+    MENU_BG_IMAGE = pygame.image.load("./images/menu_bg.jpg")  # imagens para o menu
     MENU_BG_IMAGE = pygame.transform.scale(MENU_BG_IMAGE, (LARGURA, ALTURA))
     GAME_BG_IMAGE = pygame.image.load("./images/game_bg.png")
     GAME_BG_IMAGE = pygame.transform.scale(GAME_BG_IMAGE, (LARGURA, ALTURA))
